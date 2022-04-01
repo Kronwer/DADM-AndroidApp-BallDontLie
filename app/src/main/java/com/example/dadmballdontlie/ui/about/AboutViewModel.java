0dbf6b420b58d743ab4a1ea0d19e4545d0e498eb
Zerimarc <carlosramirez522@hotmail.com>
2022-04-01 18:13:51 +0200

    mensaje about hecho

--- app/src/main/java/com/example/dadmballdontlie/ui/about/AboutViewModel.java
+++ app/src/main/java/com/example/dadmballdontlie/ui/about/AboutViewModel.java
@@ -1,22 +1,23 @@
 package com.example.dadmballdontlie.ui.about;
 
+import android.app.Application;
 import android.content.res.Resources;
 
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.dadmballdontlie.R;
 
-public class AboutViewModel extends ViewModel {
+public class AboutViewModel extends AndroidViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public AboutViewModel() {
+    public AboutViewModel (Application application) {
+        super(application);
         mText = new MutableLiveData<>();
-        mText.setValue(Resources.getSystem().getString(R.string.about_message));
-
-        //Resources.getSystem().getString(R.string.about_message)
+        mText.setValue(application.getApplicationContext().getString(R.string.about_message));
     }
 
     public LiveData<String> getText() {
