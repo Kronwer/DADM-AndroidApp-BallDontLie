c0d0569ad3d96d41606ed00613e8cc09a25ad791
chris <christiangarcia11598@gmail.com>
2022-04-22 22:36:12 +0200

    Player profile finished

--- app/src/main/java/com/example/dadmballdontlie/ui/player/PlayerFragment.java
+++ app/src/main/java/com/example/dadmballdontlie/ui/player/PlayerFragment.java
@@ -1,7 +1,12 @@
 package com.example.dadmballdontlie.ui.player;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -26,7 +31,6 @@ public class PlayerFragment extends Fragment {
     private FragmentPlayerBinding binding;
     private SharedViewModel sharedViewModel;
 
-    private PlayerAdapter adapter;
     private Stat stat = new Stat();
 
     @Override
@@ -39,81 +43,62 @@ public class PlayerFragment extends Fragment {
         sharedViewModel = new ViewModelProvider(requireActivity(),
                 new SharedViewModelFactory(requireActivity().getApplication())).get(SharedViewModel.class);
 
-        String position = changePosition(getPlayerPosition());
+        displayPlayerInfo();
+        setListeners();
+        setObservers();
 
+        return root;
+
+    }
+
+    private void displayPlayerInfo() {
+
+        // Set player name
         binding.nameTextView.setText(getPlayerName());
+
+        // Set player position
+        String position = changePosition(getPlayerPosition());
+        if(position.equals("")) {
+            binding.personIcon.setVisibility(View.GONE);
+        }
         binding.positionTextView.setText(position);
-        binding.heightTextView.setText(getPlayerHeight() + "m");
-        binding.weightTextView.setText(getPlayerWeight() + "kg");
-        binding.teamTextView.setText(getPlayerTeam());
-        loadTeamImage(binding, getPlayerTeam());  // Añadir icono del equipo
 
+        // Units to be used (ENG/INT)
+        String unitSystem = getString(R.string.units);
 
-        binding.textViewStatMin.setText("0");
-        binding.textViewStatPts.setText("0");
-        binding.textViewStatAst.setText("0");
-        binding.textViewStatStl.setText("0");
-        binding.textViewStatTurnover.setText("0");
-        binding.textViewStatBlk.setText("0");
-        binding.textViewStatReb.setText("0");
-        binding.textViewStatOReb.setText("0");
-        binding.textViewStatDReb.setText("0");
-        binding.textViewStatPf.setText("0");
-        binding.textViewStatFga.setText("0");
-        binding.textViewStatFgm.setText("0");
-        binding.textViewStatFgpct.setText("0");
-        binding.textViewStatFg3a.setText("0");
-        binding.textViewStatFg3m.setText("0");
-        binding.textViewStatFg3pct.setText("0");
-        binding.textViewStatFta.setText("0");
-        binding.textViewStatFtm.setText("0");
-        binding.textViewStatFtpct.setText("0");
-        StringBuilder glossary1 = new StringBuilder();
-        glossary1.append(getString(R.string.stat_min) + "=" + getString(R.string.stat_min_def) + "\n");
-        glossary1.append(getString(R.string.stat_pts) + " = " + getString(R.string.stat_pts_def) + "\n");
-        glossary1.append(getString(R.string.stat_ast) + "=" + getString(R.string.stat_ast_def) + "\n");
-        glossary1.append(getString(R.string.stat_stl) + "=" + getString(R.string.stat_stl_def) + "\n");
-        glossary1.append(getString(R.string.stat_turnover) + "=" + getString(R.string.stat_turnover_def) + "\n");
-        glossary1.append(getString(R.string.stat_blk) + "=" + getString(R.string.stat_blk_def) + "\n");
-        glossary1.append(getString(R.string.stat_reb) + "=" + getString(R.string.stat_reb_def) + "\n");
-        glossary1.append(getString(R.string.stat_oreb) + "=" + getString(R.string.stat_oreb_def) + "\n");
-        glossary1.append(getString(R.string.stat_dreb) + "=" + getString(R.string.stat_dreb_def) + "\n");
-        glossary1.append(getString(R.string.stat_pf) + "=" + getString(R.string.stat_pf_def) + "\n");
-        binding.textViewGlossary1.setText(glossary1);
+        // Set player height
+        binding.heightTextView.setText(getPlayerHeight(unitSystem));
 
-        binding.buttonVisibility1.setOnClickListener(new View.OnClickListener() {
+        // Set player weight
+        binding.weightTextView.setText(getPlayerWeight(unitSystem));
+
+        // Set player's team logo
+        binding.imageViewPlayer.setImageResource(getPlayerTeamLogo());
+
+    }
+
+    private void setListeners() {
+
+        binding.glossaryButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public void onClick(View view) {
-                if (binding.textViewGlossary1.getVisibility() == View.INVISIBLE) binding.textViewGlossary1.setVisibility(View.VISIBLE);
-                else if (binding.textViewGlossary1.getVisibility() == View.VISIBLE) binding.textViewGlossary1.setVisibility(View.INVISIBLE);
+            public void onClick(View v) {
+                Glossary glossaryFragment = new Glossary();
+                glossaryFragment.show(requireActivity().getSupportFragmentManager(), getString(R.string.stat_glossary));
             }
+
         });
 
-        StringBuilder glossary2 = new StringBuilder();
-        glossary2.append(getString(R.string.stat_fga) + "=" + getString(R.string.stat_fga_def) + "\n");
-        glossary2.append(getString(R.string.stat_fgm) + "=" + getString(R.string.stat_fgm_def) + "\n");
-        glossary2.append(getString(R.string.stat_fg_pct) + "=" + getString(R.string.stat_fg_pct) + "\n");
-        glossary2.append(getString(R.string.stat_fg3a) + "=" + getString(R.string.stat_fg3a_def) + "\n");
-        glossary2.append(getString(R.string.stat_fg3m) + "=" + getString(R.string.stat_fg3m_def) + "\n");
-        glossary2.append(getString(R.string.stat_fg3_pct) + "=" + getString(R.string.stat_fg3_pct_def) + "\n");
-        glossary2.append(getString(R.string.stat_fta) + "=" + getString(R.string.stat_fta_def) + "\n");
-        glossary2.append(getString(R.string.stat_ftm) + "=" + getString(R.string.stat_ftm_def) + "\n");
-        glossary2.append(getString(R.string.stat_ft_pct) + "=" + getString(R.string.stat_ft_pct_def) + "\n");
-        binding.textViewGlossary2.setText(glossary2);
+    }
 
-        binding.buttonVisibility2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (binding.textViewGlossary2.getVisibility() == View.INVISIBLE) binding.textViewGlossary2.setVisibility(View.VISIBLE);
-                else if (binding.textViewGlossary2.getVisibility() == View.VISIBLE) binding.textViewGlossary2.setVisibility(View.INVISIBLE);
-            }
-        });
+    private void setObservers() {
 
         sharedViewModel.stat.observe(getViewLifecycleOwner(), new Observer<Stat>() {
             @Override
             public void onChanged(Stat stat) {
                 if (stat != null && stat.getSeason() == 2021){
                     setStat(stat);
+                    binding.seasonTextView.setText(getString(R.string.stat_season_stats,stat.getSeason()+""));
                     binding.textViewStatMin.setText(stat.getMinutes());
                     binding.textViewStatPts.setText(stat.getPoints()+"");
                     binding.textViewStatAst.setText(stat.getAssists()+"");
@@ -133,31 +118,31 @@ public class PlayerFragment extends Fragment {
                     binding.textViewStatFta.setText(stat.getFreeThrowsAttempted()+"");
                     binding.textViewStatFtm.setText(stat.getFreeThrowsMade()+"");
                     binding.textViewStatFtpct.setText(stat.getFreeThrowsPercentage()+"");
-                }else{
-                    binding.textViewStatMin.setText("0");
-                    binding.textViewStatPts.setText("0");
-                    binding.textViewStatAst.setText("0");
-                    binding.textViewStatStl.setText("0");
-                    binding.textViewStatTurnover.setText("0");
-                    binding.textViewStatBlk.setText("0");
-                    binding.textViewStatReb.setText("0");
-                    binding.textViewStatOReb.setText("0");
-                    binding.textViewStatDReb.setText("0");
-                    binding.textViewStatPf.setText("0");
-                    binding.textViewStatFga.setText("0");
-                    binding.textViewStatFgm.setText("0");
-                    binding.textViewStatFgpct.setText("0");
-                    binding.textViewStatFg3a.setText("0");
-                    binding.textViewStatFg3m.setText("0");
-                    binding.textViewStatFg3pct.setText("0");
-                    binding.textViewStatFta.setText("0");
-                    binding.textViewStatFtm.setText("0");
-                    binding.textViewStatFtpct.setText("0");
+                } else {
+                    binding.seasonTextView.setText(getString(R.string.stat_season_stats,""));
+                    String defaultValue = getString(R.string.stat_default_value);
+                    binding.textViewStatMin.setText(defaultValue);
+                    binding.textViewStatPts.setText(defaultValue);
+                    binding.textViewStatAst.setText(defaultValue);
+                    binding.textViewStatStl.setText(defaultValue);
+                    binding.textViewStatTurnover.setText(defaultValue);
+                    binding.textViewStatBlk.setText(defaultValue);
+                    binding.textViewStatReb.setText(defaultValue);
+                    binding.textViewStatOReb.setText(defaultValue);
+                    binding.textViewStatDReb.setText(defaultValue);
+                    binding.textViewStatPf.setText(defaultValue);
+                    binding.textViewStatFga.setText(defaultValue);
+                    binding.textViewStatFgm.setText(defaultValue);
+                    binding.textViewStatFgpct.setText(defaultValue);
+                    binding.textViewStatFg3a.setText(defaultValue);
+                    binding.textViewStatFg3m.setText(defaultValue);
+                    binding.textViewStatFg3pct.setText(defaultValue);
+                    binding.textViewStatFta.setText(defaultValue);
+                    binding.textViewStatFtm.setText(defaultValue);
+                    binding.textViewStatFtpct.setText(defaultValue);
                 }
             }
         });
-
-        return root;
 
     }
 
@@ -171,20 +156,44 @@ public class PlayerFragment extends Fragment {
         return getArguments().getString("position");
     }
 
-    private String getPlayerHeight() {
-        double heightFeet = getArguments().getInt("height_feet") * FEET_TO_M;
-        double heightInches = getArguments().getInt("height_inches") * INCHES_TO_M;
-        return String.format("%.2f", heightFeet + heightInches);
+    private String getPlayerHeight(String unitSystem) {
+
+        double heightFeet = getArguments().getInt("height_feet");
+        double heightInches = getArguments().getInt("height_inches");
+
+        if(unitSystem.equals("ENG")) { // English System (Feet + inches)
+
+            return getString(R.string.height_unit_eng, String.format("%.0f", heightFeet), String.format("%.0f", heightInches));
+
+        } else {
+
+            heightFeet *= FEET_TO_M;
+            heightInches *= INCHES_TO_M;
+
+            return getString(R.string.height_unit_int, String.format("%.2f", heightFeet + heightInches));
+
+        }
+
     }
 
-    private String getPlayerWeight() {
-        double weightPounds = getArguments().getInt("weight_pounds") * POUND_TO_KG;
-        return String.format("%.2f", weightPounds);
+    private String getPlayerWeight(String unitSystem) {
+
+        double weightPounds = getArguments().getInt("weight_pounds");
+
+        if(unitSystem.equals("ENG")) {
+
+            return getString(R.string.weight_unit_eng, String.format("%.0f", weightPounds));
+
+        } else {
+
+            return getString(R.string.weight_unit_int, String.format("%.1f", weightPounds * POUND_TO_KG));
+
+        }
+
     }
 
-    private String getPlayerTeam() {
-        String team = getArguments().getBundle("team").getString("name");
-        return team;
+    private int getPlayerTeamLogo() {
+        return getArguments().getBundle("team").getInt("team_logo");
     }
 
     private Stat getStat(){
@@ -196,57 +205,30 @@ public class PlayerFragment extends Fragment {
     }
 
     private String changePosition(String pos){
-        String aux = "-";
 
         switch (pos){
-            case "C":
-               aux =  getString(R.string.position_C);
-                break;
-            case "F":
-                aux = getString(R.string.position_F);
-                break;
-            case "G":
-                aux = getString(R.string.position_G);
-                break;
+            case "C": return getString(R.string.position_C);
+            case "F": return getString(R.string.position_F);
+            case "G": return getString(R.string.position_G);
+            default: return "";
         }
-        return aux;
+
     }
 
-    private void loadTeamImage(FragmentPlayerBinding binding,String team){
-        switch (team){
-            case "Bucks": binding.imageViewPlayer.setImageDrawable(getResources().getDrawable(R.drawable.ic_bucks)); break;
-            case "Bulls": binding.imageViewPlayer.setImageDrawable(getResources().getDrawable(R.drawable.ic_bulls)); break;
-            case "Cavaliers": binding.imageViewPlayer.setImageDrawable(getResources().getDrawable(R.drawable.ic_cavaliers)); break;
-            case "Celtics": binding.imageViewPlayer.setImageDrawable(getResources().getDrawable(R.drawable.ic_celtics)); break;
-            case "Clippers": binding.imageViewPlayer.setImageDrawable(getResources().getDrawable(R.drawable.ic_clippers)); break;
-            case "Grizzlies": binding.imageViewPlayer.setImageDrawable(getResources().getDrawable(R.drawable.ic_grizzlies)); break;
-            case "Hawks": binding.imageViewPlayer.setImageDrawable(getResources().getDrawable(R.drawable.ic_hawks)); break;
-            case "Heat": binding.imageViewPlayer.setImageDrawable(getResources().getDrawable(R.drawable.ic_heat)); break;
-            case "Hornets": binding.imageViewPlayer.setImageDrawable(getResources().getDrawable(R.drawable.ic_hornets)); break;
-            case "Jazz": binding.imageViewPlayer.setImageDrawable(getResources().getDrawable(R.drawable.ic_jazz)); break;
-            case "Kings": binding.imageViewPlayer.setImageDrawable(getResources().getDrawable(R.drawable.ic_kings)); break;
-            case "Knicks": binding.imageViewPlayer.setImageDrawable(getResources().getDrawable(R.drawable.ic_knicks)); break;
-            case "Lakers": binding.imageViewPlayer.setImageDrawable(getResources().getDrawable(R.drawable.ic_lakers)); break;
-            case "Magic": binding.imageViewPlayer.setImageDrawable(getResources().getDrawable(R.drawable.ic_magic));  break;
-            case "Mavericks": binding.imageViewPlayer.setImageDrawable(getResources().getDrawable(R.drawable.ic_maverick)); break;
-            case "Nets": binding.imageViewPlayer.setImageDrawable(getResources().getDrawable(R.drawable.ic_nets)); break;
-            case "Nuggets": binding.imageViewPlayer.setImageDrawable(getResources().getDrawable(R.drawable.ic_nuggets)); break;
-            case "Pacers": binding.imageViewPlayer.setImageDrawable(getResources().getDrawable(R.drawable.ic_pacers)); break;
-            case "Pelicans": binding.imageViewPlayer.setImageDrawable(getResources().getDrawable(R.drawable.ic_pelicans)); break;
-            case "Pistons": binding.imageViewPlayer.setImageDrawable(getResources().getDrawable(R.drawable.ic_pistons)); break;
-            case "Raptors": binding.imageViewPlayer.setImageDrawable(getResources().getDrawable(R.drawable.ic_raptors)); break;
-            case "Rockets": binding.imageViewPlayer.setImageDrawable(getResources().getDrawable(R.drawable.ic_rockets)); break;
-            case "76ers": binding.imageViewPlayer.setImageDrawable(getResources().getDrawable(R.drawable.ic_sixers)); break;
-            case "Spurs": binding.imageViewPlayer.setImageDrawable(getResources().getDrawable(R.drawable.ic_spurs)); break;
-            case "Suns":  binding.imageViewPlayer.setImageDrawable(getResources().getDrawable(R.drawable.ic_suns)); break;
-            case "Thunder": binding.imageViewPlayer.setImageDrawable(getResources().getDrawable(R.drawable.ic_thunders)); break;
-            case "Timberwolves": binding.imageViewPlayer.setImageDrawable(getResources().getDrawable(R.drawable.ic_timberwolves)); break;
-            case "Trail Blazers": binding.imageViewPlayer.setImageDrawable(getResources().getDrawable(R.drawable.ic_trailblazers)); break;
-            case "Warriors": binding.imageViewPlayer.setImageDrawable(getResources().getDrawable(R.drawable.ic_warriors)); break;
-            case "Wizards": binding.imageViewPlayer.setImageDrawable(getResources().getDrawable(R.drawable.ic_wizards)); break;
+    public static class Glossary extends DialogFragment {
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            // Get the layout inflater
+            LayoutInflater inflater = requireActivity().getLayoutInflater();
+
+            // Inflate and set the layout for the dialog
+            // Pass null as the parent view because its going in the dialog layout
+            builder.setView(inflater.inflate(R.layout.fragment_glossary, null));
+
+            return builder.create();
         }
     }
-
-
 
 }
